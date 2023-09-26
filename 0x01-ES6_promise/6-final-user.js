e2d9f194b86e7878d6b1e2f64b31e6f711ff0545
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-09-26 19:59:55 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,16 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  const results = [];
-  
-  return Promise.allSettled(promises).then((results) => {
-    results.map(({ status, value, reason }) => (
-      result.push({
-        status,
-        value: status === 'rejected' ? reason.toString() : value,
-      })
-    ));
-    return results;
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => {
+    return results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }));
   });
 }
