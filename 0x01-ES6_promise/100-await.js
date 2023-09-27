ab301df0218c68f19820953e258ccd609970f088
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-09-27 16:47:10 +0300

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,6 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   let photo;
   let user;
+
   try {
     photo = await uploadPhoto();
     user = await createUser();
@@ -10,5 +11,6 @@ export default async function asyncUploadUser() {
     photo = null;
     user = null;
   }
+
   return { photo, user };
 }
