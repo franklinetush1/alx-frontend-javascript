5b16f8a830ca3e1d1e17cfab5eaae001221261ae
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-09-27 16:45:43 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ])
     .then((results) => results.map(({ status, value, reason }) => ({
       status,
