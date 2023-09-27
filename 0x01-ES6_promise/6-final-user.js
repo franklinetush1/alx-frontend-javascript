2638ce8fbeea1af78e34a71d99ba92565cd84d96
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-09-27 16:33:55 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -11,5 +11,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
         status,
         value: status === 'rejected' ? reason.toString() : value,
       })
-    ));
+    )));
 }
