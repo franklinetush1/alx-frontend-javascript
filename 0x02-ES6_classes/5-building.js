4a643f9a8b69993c0d6149102e2d3a0c2e19c259
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-09-28 17:43:29 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,4 +1,4 @@
-class Building {
+export default class Building {
   constructor(sqft) {
    
     if (typeof sqft !== 'number') {
@@ -16,5 +16,3 @@ class Building {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
-
-export Default Building;
