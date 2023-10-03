ea821ad4f700974df48f82078780f854a5e437b2
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-10-03 19:22:32 +0300

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,15 @@
-export default function signUpUser(firstName, lastName) {
-	return new Promise((resolve,reject) => {
-		if (firstName && lastName){
-			resolve({
-			  firstName,
-			  lastName,
-			});
-		} else {
-			reject(new Error());
-		}
-	})
+import getListStudents from "./0-get_list_students.js";
+
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      if (gradeObj) {
+        student.grade = gradeObj.grade;
+      } else {
+        student.grade = 'N/A';
+      }
+      return student;
+    });
 }
