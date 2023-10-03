68a7b407c035c78135de38ad44caafb48a607fe4
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-10-03 22:08:46 +0300

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
-import getListStudents from './0-get_list_students.js';
-
 export default function getStudentIdsSum(arr) {
   const sum = arr.reduce((accumulator, student) => accumulator + student.id, 0);
   return sum;
