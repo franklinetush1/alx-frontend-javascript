7020e4421b9127d91c96e78100825e273b06e29e
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-10-03 22:20:57 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !startString.length || typeof startString !== 'string') return '';
-
-  const arr = [];
-  for (const item of set) {
-    if (item.startsWith(startString)) {
-      arr.push(item.substring(startString.length));
-    }
-  }
-  return arr.join('-');
+  let resultStr = '';
+  if (!startString || !startString.length) return resultStr;
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) resultStr += `${element.slice(startString.length)}-`;
+  });
+  return resultStr.slice(0, resultStr.length - 1);
+}
