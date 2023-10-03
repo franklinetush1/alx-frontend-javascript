c305dedc539a8412056c896f821d3570e41f3705
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-10-03 18:44:28 +0300

    Update 10-update_uniq_items.js

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(inputMap) {
+  if (!(inputMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [item, quantity] of inputMap.entries()) {
+    if (quantity === 1) {
+      inputMap.set(item, 100);
+    }
+  }
+}
