992981bfdb1a22dc371cff3ba196be5a0431a949
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-10-03 18:37:44 +0300

    Update 2-get_students_by_loc.js

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+import getListStudents from "./0-get_list_students.js";
+
+export default function getStudentsByLocation (arr, city){
+		const filtered = arr.filter((cit) => cit.location === city);
+		return filtered;
+	}
